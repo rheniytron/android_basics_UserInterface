15817727d9f1684aa0b10ea208ae6958a85a3eea
AbdulRahmon <rheniytron0270@gmail.com>
2018-06-02 15:49:01 +0100

    test

---
+++ app/src/main/java/com/rheniytron/helloandroid/and_basics2/Main.java
@@ -0,0 +1,13 @@
+package com.rheniytron.helloandroid.and_basics2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day_7_login_box);
+    }
+}
